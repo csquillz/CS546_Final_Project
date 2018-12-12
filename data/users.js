1774bf879213e496fadc144b4781c058a5726572
csquillz <carolinesquillante@gmail.com>
2018-12-12 18:49:40 -0500

    added db func to keep track of session ids

--- data/users.js
+++ data/users.js
@@ -12,7 +12,7 @@ async function addUser(firstName, lastName, userName, password) {
     let newUser = {
       _id: uuid.v4(),
       username: userName.toLowerCase(),
-      sesssioId: uuid.v4(),
+      sessionId: "",
       hashedPassword: hash,
       Account: {
         firstName: firstName.toLowerCase(),
@@ -33,7 +33,6 @@ async function addUser(firstName, lastName, userName, password) {
   });
 }
 
-
 async function checkForExistingUser(username) {
   if (!username) throw "Invalid username";
   return users().then(usersCollection => {
@@ -46,40 +45,40 @@ async function checkForExistingUser(username) {
 
 async function validatePassword(username, password) {
   const userInfo = await checkForExistingUser(username);
-  console.log(userInfo)
-  console.log(userInfo.hashedPassword)
+  // console.log(userInfo)
+  // console.log(userInfo.hashedPassword)
   //console.log(password)
   let compareToMatch = false
-  // try {
-  compareToMatch = await bcrypt.compare(password, String(userInfo.hashedPassword))
-  // } catch (e) {
-  //   throw (e)
-  // }
+  try {
+    compareToMatch = await bcrypt.compare(password, String(userInfo.hashedPassword))
+  } catch (e) {
+    throw (e)
+  }
   return compareToMatch;
 }
 
-async function getInfo(username) {
-  for (let user of users) {
-    if (user.username === username) {
-      return {
-        _id: user._id,
-        username: user.username,
-        hashedPassword: user.hashedPassword,
-        Account: {
-          _id: user._id,
-          firstName: user.firstName,
-          lastName: user.lastName
-        }
-      }
-    }
-  }
-}
+// async function getInfo(username) {
+//   for (let user of users) {
+//     if (user.username === username) {
+//       return {
+//         _id: user._id,
+//         username: user.username,
+//         hashedPassword: user.hashedPassword,
+//         Account: {
+//           _id: user._id,
+//           firstName: user.firstName,
+//           lastName: user.lastName
+//         }
+//       }
+//     }
+//   }
+// }
 
 async function validateSessionId(sessionId) {
-  if (!sessionId) throw "Invalid session Id";
+  //if (!sessionId) throw "Invalid session Id";
   return users().then(usersCollection => {
     return usersCollection.findOne({ sessionId: sessionId }).then(account => {
-      if (!account) throw "username not found";
+      //if (!account) throw "Session Id not found";
       return account;
     });
   });
@@ -89,27 +88,35 @@ async function getUserInfoById(sessionId) {
   validateSessionId(sessionId)
 }
 
-async function addSessionId(sessionId, username) {
-  // const userCollection = await recipes();
+async function addSessionId(newsessionId, username) {
+  const usersCollection = await users();
 
-  // const updatedInfo = await userCollection.updateOne({ username: username }, { $set: updatedUser });
-
-  // if (updatedInfo.modifiedCount === 0) {
-  //   throw "could not update recipe successfully";
-  // }
-  return await this.getUserInfoById(id);
-
-  // userSessionIds.push({ sessionId: sessionId, username: username })
-  // return userSessionIds
+  //console.log(username);
+  //console.log(sessionId);
+  // console.log(await checkForExistingUser(username));
+  //const updatedUser =  await usersCollection.findOne({ username: username })
+  const updatedInfo = await usersCollection.updateOne({ username: username }, { $set: {sessionId: newsessionId}});
+  //console.log(updatedUser)
+  if (updatedInfo.modifiedCount === 0) {
+    throw "could not update session id successfully";
+  }
+  //console.log(await checkForExistingUser(username));
+  return await this.checkForExistingUser(username);
 }
 
 async function removeSessionId(sessionId) {
-  return await this.getUserInfoById(id);
- 
-  // userSessionIds.filter(function (id) { return id.sessionId !== sessionId });
-  // return userSessionIds;
+
+  const userCollection = await users();
+
+  const updatedInfo = await userCollection.updateOne({ sessionId: sessionId }, { $set: {sessionId: null }});
+
+  if (updatedInfo.modifiedCount === 0) {
+    throw "could not update user successfully";
+  }
+
+  return await this.getUserInfoById(sessionId);
 }
 
 
-let exportedMethods = { addUser, checkForExistingUser, validatePassword, getInfo, validateSessionId, getUserInfoById, addSessionId, removeSessionId }
+let exportedMethods = { addUser, checkForExistingUser, validatePassword, validateSessionId, getUserInfoById, addSessionId, removeSessionId }
 module.exports = exportedMethods;
