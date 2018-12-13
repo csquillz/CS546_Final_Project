202f36649d4ed1c3389bbf8f18bb4c431f4376a6
csquillz <carolinesquillante@gmail.com>
2018-12-13 17:00:40 -0500

    fix user route

--- routes/users.js
+++ routes/users.js
@@ -115,7 +115,7 @@ router.get("/private", async function (req, res) {
     const sessionId = req.cookies.sessionId;
     const user = await usersData.getUserInfoById(sessionId);
     let goalData = await goalsData.getAllGoals(user.Account.userName)
-    console.log(goalData)
+    console.log(goalData);
     let transactionData = [];
     res.render("pages/private", {
         firstName: user.Account.firstName,
